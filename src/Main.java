932e7615e47563ad03f66dd926a2218969a47967
Marius <marius.sautiut@brillio.com>
2022-10-07 15:55:50 +0300

    Tema laborator 29 Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //1.Printati "Hello" in consola, iar pe rand nou, printati numele vostru.//
+
+        System.out.println("Hello \nMarius!");
+
+        //2.Printati rezultatul sumei a doua numere(orice numere)//
+        //System.out.println("Suma a doua numere 2 + 2 este:");//
+        int SumaADouaNumere = 2 + 2;
+        System.out.println(SumaADouaNumere);
+
+        //3. Printati rezultatul impartirii a doua numere. Atentie, daca numarul nu este intreg, sa afiseze si valorile dupa virgula.//
+        //System.out.println("Impartirea a doua numere 10/3 este:");//
+        double ImpartireaADouaNumere = 10/3d;
+        System.out.println(ImpartireaADouaNumere);
+
+        //4.Printati rezultatul urmatoarelor operatiuni//
+        //a.//
+        int OperatiuneaA = -5 + 8 * 6;
+        System.out.println(OperatiuneaA);
+
+        //b.//
+        double OperatiuneaB = (55+9) % 9d;
+        System.out.println(OperatiuneaB);
+
+        //c.//
+        int OperatiuneaC =20 + -3*5 / 8;
+        System.out.println(OperatiuneaC);
+
+        //d.//
+        double OperatiuneaD = 5 + 15 / 3 * 2 - 8 % 3d;
+        System.out.println(OperatiuneaD);
+
+
+
+
     }
 }
