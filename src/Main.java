93374ca6663b2f5935c4da8a070d442ff800b115
Marius <marius.sautiut@brillio.com>
2022-10-30 20:25:42 +0200

    Tema laborator 29 Java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
          // Tema Lab9 Introducere in programare java //
 
@@ -33,40 +37,74 @@ public class Main {
 //        double OperatiuneaD = 5 + 15 / 3 * 2 - 8 % 3d;
 //        System.out.println(OperatiuneaD);
 
-        //Tema Lab10 Metoda si Obiecte //
-        //1.Pentru exercitiile din clasa main() de data trecuta, creati metode separate pentru fiecare, tot in clasa Main.java, si apoi apelati-le in metoda main().//
-        printMyName();
-        sumaADouaNumere();
-        impartireaADouaNumere();
-        OperatiuneaA();
-        OperatiuneaB();
-        OperatiuneaC();
-        OperatiuneaD();
+        Calculator calc = new Calculator();
 
-        //2.Scrieti care o metoda java pentru fiecare din urmatoarele operatii matematice: adunare, scadere, inmultire, impartire.//
-        System.out.println("rezultatul Adunarii este :" + adunarea(2,3));
-        System.out.println("rezultatul Scaderii este :" + scaderea(10,5));
-        System.out.println("rezultatul Inmultirii este :" + inmultirea(27,3));
-        System.out.println("rezultatul Impartirii este :" + impartirea(1350,70));
 
-        //3.Scrieti o metoda java, care sa afiseze urmatorul model//
-        modelJava();
+//        //Tema Lab10 Metoda si Obiecte //
+//        //1.Pentru exercitiile din clasa main() de data trecuta, creati metode separate pentru fiecare, tot in clasa Main.java, si apoi apelati-le in metoda main().//
+//        calc.printMyName();
+//        calc.sumaADouaNumere();
+//        calc.impartireaADouaNumere();
+//        calc.OperatiuneaA();
+//        calc.OperatiuneaB();
+//        calc.OperatiuneaC();
+//        calc.OperatiuneaD();
+//
+//        //2.Scrieti care o metoda java pentru fiecare din urmatoarele operatii matematice: adunare, scadere, inmultire, impartire.//
+//        System.out.println("rezultatul Adunarii este :" + calc.adunarea(2,3));
+//        System.out.println("rezultatul Scaderii este :" + calc.scaderea(10,5));
+//        System.out.println("rezultatul Inmultirii este :" + calc.inmultirea(27,3));
+//        System.out.println("rezultatul Impartirii este :" + calc.impartirea(1350,70));
+//
+//        //3.Scrieti o metoda java, care sa afiseze urmatorul model//
+//        calc.modelJava();
+//
+//        //4.//
+//
+//        System.out.println(calc.mediaATreiNumere(10,10,11));
+//
+//        //5.Scrieti o metoda java, care sa afiseze urmatorul model://
+//        calc.chipDeOm();
+//
+//        //6.Scrieti o metoda java, care sa returneze restul impartirii a doua numere//
+//        calc.restulImpartirii();
+//
+//        //7.Scrieti o metoda java, care sa primeasca ca si parametru un numar, care sa reprezinte temperatura in Fahrenheit, si sa returneze temperatura in Celsius.//
+//        System.out.println(calc.celsius(50));
+//
+//        //8.Scrieti o metoda java, care sa primeasca ca si parametru un numar real, care sa reprezinte distanta in inch, si sa returneze distanta in metrii??
+//        System.out.println(calc.meters(500));
 
-        //4.//
+//        //9.//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        float timeSeconds;
+//        float mps,kph, mph;
+//
+//        System.out.print("Input distance in meters: ");
+//        float distance = scanner.nextFloat();
+//
+//        System.out.print("Input hour: ");
+//        float hr = scanner.nextFloat();
+//
+//        System.out.print("Input minutes: ");
+//        float min = scanner.nextFloat();
+//
+//        System.out.print("Input seconds: ");
+//        float sec = scanner.nextFloat();
+//
+//        timeSeconds = (hr*3600) + (min*60) + sec;
+//        mps = distance / timeSeconds;
+//        kph = ( distance/1000.0f ) / ( timeSeconds/3600.0f );
+//        mph = kph / 1.609f;
+//
+//        System.out.println("Your speed in meters/second is "+mps);
+//        System.out.println("Your speed in km/h is "+kph);
+//        System.out.println("Your speed in miles/h is "+mph);
+//
+//
+//        scanner.close();
 
-        System.out.println(mediaATreiNumere(10,10,11));
-
-        //5.Scrieti o metoda java, care sa afiseze urmatorul model://
-        chipDeOm();
-
-        //6.Scrieti o metoda java, care sa returneze restul impartirii a doua numere//
-        restulImpartirii();
-
-        //7.Scrieti o metoda java, care sa primeasca ca si parametru un numar, care sa reprezinte temperatura in Fahrenheit, si sa returneze temperatura in Celsius.//
-        System.out.println(celsius(50));
-
-        //8.Scrieti o metoda java, care sa primeasca ca si parametru un numar real, care sa reprezinte distanta in inch, si sa returneze distanta in metrii??
-        System.out.println(meters(500));
 
 
 //        int x = 2;
@@ -84,103 +122,76 @@ public class Main {
 //        float d= asterics(x,y);
 //        System.out.println(d);
 
-    }
+        //TEMA LAB IF / ELSE
 
-    //Tema Lab10//
-    //1.//
-    public static void printMyName() {
-        System.out.println("Hello \nMarius!");
-    }
-    public static void sumaADouaNumere(){
-        int SumaADouaNumere = 2 + 2;
-        System.out.println(SumaADouaNumere);
-    }
-    public static void impartireaADouaNumere(){
-        double ImpartireaADouaNumere = 10/3d;
-        System.out.println(ImpartireaADouaNumere);
-    }
-    public static void OperatiuneaA(){
-        int OperatiuneaA = -5 + 8 * 6;
-        System.out.println(OperatiuneaA);
-    }
-    public static void OperatiuneaB(){
-        double OperatiuneaB = (55+9) % 9d;
-        System.out.println(OperatiuneaB);
-    }
-    public static void OperatiuneaC(){
-        int OperatiuneaC =20 + -3*5 / 8;
-        System.out.println(OperatiuneaC);
-    }
-    public static void OperatiuneaD(){
-        double OperatiuneaD = 5 + 15 / 3 * 2 - 8 % 3d;
-        System.out.println(OperatiuneaD);
-    }
+//        // Exercitiul 3
+        LogicalOperations op = new LogicalOperations();
+//        int biggest= op.checkBiggerNumber(4,3);
+//        System.out.println("The bigger number is:" + biggest);
+//
+//        // Exercitiul 4
+//        String fastTrack = op.textComparison("FastTrackIT");
+//        System.out.println(fastTrack);
+//
+//        // Exercitiul 5
+//        String exercitiulTemaCinci =op.exercitiulCinci("Burger", 5);
+//        System.out.println(exercitiulTemaCinci);
+//
+//        // Exercitiul 6
+//        String exercitiulTemaSase =op.exercitiulSase(5);
+//        System.out.println(exercitiulTemaSase);
+//
+//        //Exercitiul 7
+//        String exercitiulTemaSapte = op.exercitiulSapte(4);
+//        System.out.println(exercitiulTemaSapte);
+//
+//        //Exercitiul 9
+//        boolean exercitiulTemaNoua = op.isNumberEven(22);
+//        System.out.println(exercitiulTemaNoua);
+//
+//        //Exercitiul 10
+//        boolean exercitiulTemaZece = op.isEligibleToVote(18);
+//        System.out.println(exercitiulTemaZece);
+//
+//        //Exercitiul 11
+//        int exercitiulTemaUnsprezece = op.isGreaterNumber(21,32,8);
+//        System.out.println(exercitiulTemaUnsprezece);
 
-    //2.//
-    public static int adunarea(int primulNumar, int alDoileaNumar){
-        int rezultat= primulNumar + alDoileaNumar;
-        return rezultat;
-    }
-    public static float scaderea(float primulNumar, float alDoileaNumar){
-        float rezultat= primulNumar - alDoileaNumar;
-        return rezultat;
-    }
-    public static float inmultirea(float primulNumar, float alDoileaNumar){
-        float rezultat=primulNumar * alDoileaNumar;
-        return rezultat;
-    }
-    public static float impartirea(float primulNumar, float alDoileaNumar){
-        float rezultat=primulNumar / alDoileaNumar;
-        return rezultat;
-    }
+        //Tema Java For Loops
 
-    //3.//
-    public static void modelJava(){
-        System.out.println("    J     a   v     v  a" +
-                         "\n    J    a a   v   v  a a" +
-                         "\n J  J   aaaaa   V V  aaaaa" +
-                         "\n  JJ   a     a   V  a     a"+
-                         "\n"
-                );
+//        //Exercitiul 1
+//        op.printFromNumberTo100(10);
+//
+//
+//        //Exercitiul 2
+//        op.printFromNumberToMinus100(2);
 
-    }
+        //Exercitiul 3
 
-    //4.//
+//        op.exercitiulTrei(20,30);
 
-    public static float mediaATreiNumere(float primulNumar, float alDoileaNumar, float alTreileaNumar){
-        float rezultat=(primulNumar + alDoileaNumar + alTreileaNumar) / 3;
-        return rezultat;
+        //Exercitiul 5
+//        op.evenNumber(1);
+
+        //Exercitiul 6
+//        op.oddNumber(1);
+
+        //Exercitiul 7
+//            data viitoare
+
+        //Exercitiul 8
+        System.out.println(op.sumCount());
+
+        //Exercitiul 9
+        //System.out.println(op.sumAndAverage());
+
+
     }
 
 
 
-    //5.//
-    public static void chipDeOm(){
-        System.out.println("  +\" \" \" \" \"+"+
-                       "\n[ |  o   o  | ]" +
-                       "\n  |    ^    |" +
-                       "\n  |  ' _ '  |" +
-                       "\n  +_________+"
 
-        );
-    }
 
-    //6.//
-    public static void restulImpartirii(){
-        double restulImpartirii = 100/3d;
-        System.out.println(restulImpartirii);
-    }
-
-    //7.//
-    public static float celsius(float farenheit){
-        float rezultat= ((farenheit-32)*5)/9;
-        return rezultat;
-    }
-    //8.//
-    public static float meters(float inch){
-        float rezultat= (float) (inch * 0.0254);
-        return rezultat;
-    }
 
 //    public static int sum(int first, int second) {
 //        int result = first + second;
